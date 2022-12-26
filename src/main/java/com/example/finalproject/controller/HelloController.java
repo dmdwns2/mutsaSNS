0c376893c7388b58016fd7a6e1ef1d89acc315d1
dmdwns2 <wjsdncl321@gmail.com>
2022-12-26 09:04:22 +0900

    CICD test

--- src/main/java/com/example/finalproject/controller/HelloController.java
+++ src/main/java/com/example/finalproject/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<String> hello(){
-        return ResponseEntity.ok().body("popin");
+        return ResponseEntity.ok().body("happy_new_year");
     }
 }
 
