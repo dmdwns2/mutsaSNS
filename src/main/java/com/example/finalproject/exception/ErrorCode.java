138e34e5b25c71bdb831fd106ac5a24ebbcd08cf
dmdwns2 <wjsdncl321@gmail.com>
2022-12-27 13:43:30 +0900

    Error 코드 수정

--- src/main/java/com/example/finalproject/exception/ErrorCode.java
+++ src/main/java/com/example/finalproject/exception/ErrorCode.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
     @AllArgsConstructor
     @Getter
     public enum ErrorCode {
-        DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
-        INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-        USER_NOT_FOUNDED(HttpStatus.NOT_FOUND, ""),
-        NOT_FOUNDED(HttpStatus.NOT_FOUND, "plz check your userName"),
-        INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password does not match");
+        DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."),
+        USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "Not founded"),
+        INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password does not match"),
+        INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+        POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
+        DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러");
 
 
 
