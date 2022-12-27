03d92fe516a7d5164599199697ad5db78c287495
dmdwns2 <wjsdncl321@gmail.com>
2022-12-27 17:15:35 +0900

    테스트 코드 작성

--- src/test/java/com/example/finalproject/controller/PostRestControllerTest.java
+++ src/test/java/com/example/finalproject/controller/PostRestControllerTest.java
@@ -2,76 +2,38 @@ package com.example.finalproject.controller;
 
 import com.example.finalproject.domain.dto.PostAddRequest;
 import com.example.finalproject.domain.dto.PostAddResponse;
-import com.example.finalproject.domain.dto.PostDto;
 import com.example.finalproject.service.PostService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebMvcTest(PostRestController.class)
 class PostRestControllerTest {
 
+    private static final String TITLE = "title";
+    private static final String BODY = "body";
+    private static final String USERNAME = "messi";
 
-    @Autowired
-    MockMvc mockMvc;
 
-    @MockBean
-    PostService postService;
+    private final PostService service;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
-    private final String USERNAME = "messi";
+    public PostRestControllerTest(PostService service) {
+        this.service = service;
+    }
 
 
 
     // GET /posts/1 로 조회시
     //조회 성공 - id, title, body, userName 4가지 항목이 있는지 검증
     @Test
-    void ID조회_확인() throws Exception {
-        Long id = 1l;
+    void ID조회_확인() {
 
-        given(postService.getPostById(id))
-                .willReturn(new PostDto(1l, "첫번째 글", "내용입니다.", USERNAME, any(), any()));
-
-        mockMvc.perform(get("/api/v1/posts/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.title").exists())
-                .andExpect(jsonPath("$.body").exists())
-                .andDo(print());
-
-        verify(postService).getPostById(id);
     }
 
     @Test
-    void ADD_동작_확인() throws Exception {
-        PostAddRequest dto = new PostAddRequest("제목입니다.", "내용입니다.");
-        given(postService.add(any(), any())).willReturn(new PostAddResponse(1l, dto.getTitle(), dto.getBody(), any(), "테스트"));
-
-        mockMvc.perform(post("/api/v1/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(dto))
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.title").exists())
-                .andExpect(jsonPath("$.title").value("제목입니다."))
-                .andExpect(jsonPath("$.body").exists())
-                .andDo(print());
+    void ADD_동작_확인()  {
+        PostAddResponse addResult = service.add(new PostAddRequest(TITLE, BODY), USERNAME);
+        assertEquals(TITLE, addResult.getTitle());
+        assertEquals(BODY, addResult.getBody());
     }
 
     @Test
