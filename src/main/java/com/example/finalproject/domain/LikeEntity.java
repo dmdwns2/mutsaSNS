2f4e2c7f1005de8e0bbb031bfb0ff37175a8287d
dmdwns2 <wjsdncl321@gmail.com>
2023-01-10 19:56:16 +0900

    User Join 에러

--- src/main/java/com/example/finalproject/domain/LikeEntity.java
+++ src/main/java/com/example/finalproject/domain/LikeEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 public class LikeEntity extends BaseEntity{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/example/finalproject/domain/PostEntity.java
+++ src/main/java/com/example/finalproject/domain/PostEntity.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,7 +20,7 @@ import javax.validation.constraints.NotNull;
 public class PostEntity extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     @NotNull
