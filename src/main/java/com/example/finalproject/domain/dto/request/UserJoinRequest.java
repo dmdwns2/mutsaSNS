e9ac27ebf1c76e9943f56425839468107009353c
dmdwns2 <wjsdncl321@gmail.com>
2023-01-10 19:46:26 +0900

    User Join 에러

--- src/main/java/com/example/finalproject/domain/dto/request/UserJoinRequest.java
+++ src/main/java/com/example/finalproject/domain/dto/request/UserJoinRequest.java
@@ -4,14 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-
 @Builder
 @AllArgsConstructor
 @Getter
 public class UserJoinRequest {
-    @NotBlank(message = "Blank")
-    private final String userName;
-    @NotBlank(message = "Blank")
-    private final String password;
+    private String userName;
+    private String password;
 }
