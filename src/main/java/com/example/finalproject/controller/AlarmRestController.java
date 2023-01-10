b30b52f71f289e7e208857aa168d419a8bd84b54
dmdwns2 <wjsdncl321@gmail.com>
2023-01-10 11:08:57 +0900

    알람기능 구현

--- src/main/java/com/example/finalproject/controller/AlarmRestController.java
+++ src/main/java/com/example/finalproject/controller/AlarmRestController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +21,8 @@ public class AlarmRestController {
 
     @GetMapping()
     public Response<Page<AlarmDto>> alarmPage(@PageableDefault(size=20, sort="createdAt", direction = Sort.Direction.DESC)
-                                              Pageable pageable, Authentication authentication) {
-        String userName = authentication.getName();
-        Page<AlarmDto> page = service.alarmPage(pageable, userName);
+                                              Pageable pageable) {
+        Page<AlarmDto> page = service.alarmPage(pageable);
         return Response.success(page);
     }
 }
--- src/main/java/com/example/finalproject/service/AlarmService.java
+++ src/main/java/com/example/finalproject/service/AlarmService.java
@@ -1,17 +1,14 @@
 package com.example.finalproject.service;
 
 import com.example.finalproject.domain.AlarmEntity;
-import com.example.finalproject.domain.UserEntity;
 import com.example.finalproject.domain.dto.AlarmDto;
 import com.example.finalproject.repository.AlarmRepository;
-import com.example.finalproject.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,12 +16,9 @@ import java.util.stream.Collectors;
 public class AlarmService {
 
     private final AlarmRepository repository;
-    private final UserRepository userRepository;
-    public Page<AlarmDto> alarmPage(Pageable pageable, String userName) {
+    public Page<AlarmDto> alarmPage(Pageable pageable) {
 
-        Optional<UserEntity> optUser =  userRepository.findByUserName(userName);
-        UserEntity user = optUser.orElse(null);
-        Page<AlarmEntity> visits = repository.findAlarmEntitiesByUser(user, pageable);
+        Page<AlarmEntity> visits = repository.findAll(pageable);
 
         return new PageImpl<>(visits.stream()
                 .map(AlarmEntity::toResponse)
