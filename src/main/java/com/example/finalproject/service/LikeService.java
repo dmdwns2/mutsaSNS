3a95620a25f53dfad123e403cd1a90ed160ec732
dmdwns2 <wjsdncl321@gmail.com>
2023-01-10 10:09:24 +0900

    좋아요 에러 수정

--- src/main/java/com/example/finalproject/service/LikeService.java
+++ src/main/java/com/example/finalproject/service/LikeService.java
@@ -4,18 +4,28 @@ import com.example.finalproject.domain.LikeEntity;
 import com.example.finalproject.domain.PostEntity;
 import com.example.finalproject.domain.UserEntity;
 import com.example.finalproject.repository.LikeRepository;
+import com.example.finalproject.repository.PostRepository;
+import com.example.finalproject.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 @AllArgsConstructor
 public class LikeService {
 
     private final LikeRepository repository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
-    private final UserEntity user;
-    private final PostEntity post;
     public void like_on(Long postId, String userName) {
+
+        Optional<PostEntity> optPost = postRepository.findById(postId);
+        PostEntity post = optPost.orElse(null);
+        Optional<UserEntity> optUser = userRepository.findByUserName(userName);
+        UserEntity user = optUser.orElse(null);
 
         post.setId(postId);
         user.setUserName(userName);
